3f5aa9f292e0e037ec24c075955d520bd3c5d5e2
Jayden Ellisdon <Jaydelli3137@gmail.com>
2020-09-14 17:01:25 +1000

    Add files via upload

---
+++ EasyPost/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  EasyPost
+//
+//  Created by Jayden Ellisdon on 13/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //allows for tapping outside of text field to close text field editing
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, with: event)
+    }
+    
+    // allows for return to exit out of text field edtiting
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "\n" {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+}
+
---
+++ EasyPost_watch Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  EasyPost_watch Extension
+//
+//  Created by Jayden Ellisdon on 14/9/20.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
